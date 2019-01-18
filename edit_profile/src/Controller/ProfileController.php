05f7f1f724ab6741345d6a75229ac94dac936418
sander fraeyman <sanderfraeyman@gmail.com>
2019-01-18 17:03:26 +0100

    Started conversion of menu hook

---
+++ edit_profile/src/Controller/ProfileController.php
@@ -0,0 +1,22 @@
+/**
+ * @file
+ * Contains \Drupal\edit_profile\Controller\ProfileController.
+ */
+
+namespace Drupal\edit_profile\Controller;
+
+/**
+ * Controller routines for edit_profile routes.
+ */
+class ProfileController {
+
+  /**
+   * Returns an administrative overview of user profile.
+   *
+   * @return array
+   *   A render array representing the administrative page content.
+   */
+  public function profileOverview() {
+    // ...
+  }
+}
