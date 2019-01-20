d2c83a9adaa838d02d90a86b78d58eaabdc6e3ab
sander fraeyman <sanderfraeyman@gmail.com>
2019-01-20 12:47:34 +0100

    Reviewed code

--- edit_profile/src/Controller/ProfileController.php
+++ edit_profile/src/Controller/ProfileController.php
@@ -1,3 +1,4 @@
+<?php
 /**
  * @file
  * Contains \Drupal\edit_profile\Controller\ProfileController.
@@ -5,6 +6,7 @@
 
 namespace Drupal\edit_profile\Controller;
 
+#Deze controller hoort de informatie van de user op te halen en de form te kopieren die al in drupal zit onder de admin pagina's
 use Drupal\Core\Controller\ControllerBase;
 
 /**
--- edit_profile/src/Form/EditProfileForm.php
+++ edit_profile/src/Form/EditProfileForm.php
@@ -1,43 +1,76 @@
 <?php
-namespace Drupal\edit_profile\Form;
 
-use Drupal\Core\Form\ConfigFormBase;
-use Drupal\Core\Form\FormStateInterface;
+/**
+ * @file
+ * Contains \Drupal\src\Form\EditProfile
+ */
 
-class EditProfileForm extends ConfigFormBase {
+ namespace Drupal\edit_profile\Form;
 
+ use Drupal\Core\Form\ConfigFormBase;
+ use Drupal\Core\FormStateInterface;
+
+ class EditProfileForm extends ConfigFormBase{
+ /**
+  * Generereert een pagina
+  */
+  public function getFormId(){
+      return 'edit_profile';
+  }
+
+/**
+ * @{inheritdoc}
+ */
+
+  public function buildForm(array $form, FormStateInterface $form_state){
+      /**
+       * 21.45 min YT
+       */
+      //create constructor
+      $form = parent::buildForm($form, $form_state);
+        //default settins
+        $config = $this=>config('edit_profile.settings');
+
+      $form['edit_profile_firstname'] = array(
+        '#type' => 'textfield',
+        '#title' => $this->t('Firstname'),
+        '#description' => $this->t('Enter your first name'),
+        /**'#default_value' => $config->get('edit_profile_firstname'),*/
+        '#size' => 10,
+      );
+
+      $form['edit_profile_lastname'] = array(
+        '#type' => 'textfield',
+        '#title' => $this->t('Last Name'),
+        '#description' => $this->t('Enter your last name'),
+        /**'#default_value' => $config->get('edit_profile_lastname'),*/
+        '#size' => 10,
+      );
+
+      return $form;
+
+  }
+
+  public function submitForm(array $form, FormStateInterface $form_state){
+    //get the object
     /**
-     * {@inheritdoc}
-     */
-    public function getFormId() {
-        return 'editprofile_form';
+    * $label = $form_state->getValue('edit_profile_firstname');
+    * $label = $form_state->getValue('edit_profile_lastname');
+    */
+    // Set the values the user submitted in the form.
+    $config = $this->config('edit_profile.settings');
+    $config->set('edit_profile_firstname', $label)
+    ->set('edit_profile_lastname', $label)
+    ->save();
+  
+    return parent::submitForm($form, $form_state);
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function buildForm(array $form, FormStateInterface $form_state) {
-        // Form constructor.
-        $form = parent::buildForm($form, $form_state);
-        
-        #callback naar user form
+  protected function getEditableConfigNames(){
+    return [
+      'edit_profile.settings',
+    ]
+  }
 
-        return $form;
-    }
-    /**
-     * {@inheritdoc}
-     */
-    public function validateForm(array &$form, FormStateInterface $form_state) {
 
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function submitForm(array &$form, FormStateInterface $form_state) {
-        
-        #nieuwe waarden opslaan
-
-        return parent::submitForm($form, $form_state);
-    }
-}
+ }
